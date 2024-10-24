a3ddd64b47eb894d4b8606e4822d3580e426a84e
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-10-25 01:05:04 +0200

    final fix

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,40 +1,17 @@
 #!/usr/bin/python3
-"""
-0-subs.py
-
-This module queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function returns 0.
-
-Libraries used:
-            - requests
-Returns:
-    on success: int (total subscribers)
-    on failure: 0
-
-Author: Aimable
-Date: October 2024
-"""
+"""Return the number of subscribers of a given subreddit"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    The main function that fetches data
-    and returns total number of subs of a subreddit
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
 
-    Args:
-        subreddit (str): a subreddit name
-
-    Returns:
-        int: the total number of subscribers of a given subreddit
-    """
-    url = f"https://reddit.com/r/{subreddit}/about.json"
-
-    response = requests.get(url, allow_redirects=False)
     try:
-        total_subs = response.json()["data"]["subscribers"]
-        return total_subs
-    except:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
+
+    except Exception:
         return 0
