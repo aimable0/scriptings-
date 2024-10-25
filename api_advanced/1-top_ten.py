542374936f33737f5488b71b8188fd3729362a15
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-10-25 16:35:40 +0200

    test 5

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""
-Description:
-    function that fetched data from a reddit api
-    and prints titles of the first 10 article
-"""
+""" top_ten.py """
 import requests
 
 
 def top_ten(subreddit):
     """prints the titles of the first 10 hot posts listed in a subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=9".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
@@ -19,6 +15,27 @@ def top_ten(subreddit):
     for post in posts:
         print(post["data"]["title"])
 
-    posts = response.json().get("data", {}).get("children", [])
-    for item in posts:
-        print(item.get("data", {}).get("title", None))
+
+# """
+# Description:
+#     function that fetched data from a reddit api
+#     and prints titles of the first 10 article
+# """
+# import requests
+
+
+# def top_ten(subreddit):
+#     """prints the titles of the first 10 hot posts listed in a subreddit"""
+#     url = "https://www.reddit.com/r/{}/hot.json?limit=9".format(subreddit)
+#     headers = {"User-Agent": "Mozilla/5.0"}
+#     response = requests.get(url, headers=headers, allow_redirects=False)
+#     if response.status_code != 200:
+#         print(None)
+#         return
+#     posts = response.json()["data"]["children"]
+#     for post in posts:
+#         print(post["data"]["title"])
+
+#     posts = response.json().get("data", {}).get("children", [])
+#     for item in posts:
+#         print(item.get("data", {}).get("title", None))
