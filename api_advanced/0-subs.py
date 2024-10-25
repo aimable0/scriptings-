843d9abf29915062fc447619596a9fdcda738b01
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-10-25 02:10:09 +0200

    not yet.. h

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -6,12 +6,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """function that fetches number_of_subscribers"""
-    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "PostmanRuntime/7.35.0"}
 
     try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        return RESPONSE.json()["data"]["subscribers"]
-
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        subs = response.json()["data"]["subscribers"]
+        return subs
     except Exception:
         return 0
