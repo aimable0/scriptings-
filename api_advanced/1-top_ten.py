e7f4b79ee2bd7d66357c4264678f4b8faf79d593
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-10-25 16:37:34 +0200

    final test

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ import requests
 
 def top_ten(subreddit):
     """prints the titles of the first 10 hot posts listed in a subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=9".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
@@ -14,28 +14,3 @@ def top_ten(subreddit):
     posts = response.json()["data"]["children"]
     for post in posts:
         print(post["data"]["title"])
-
-
-# """
-# Description:
-#     function that fetched data from a reddit api
-#     and prints titles of the first 10 article
-# """
-# import requests
-
-
-# def top_ten(subreddit):
-#     """prints the titles of the first 10 hot posts listed in a subreddit"""
-#     url = "https://www.reddit.com/r/{}/hot.json?limit=9".format(subreddit)
-#     headers = {"User-Agent": "Mozilla/5.0"}
-#     response = requests.get(url, headers=headers, allow_redirects=False)
-#     if response.status_code != 200:
-#         print(None)
-#         return
-#     posts = response.json()["data"]["children"]
-#     for post in posts:
-#         print(post["data"]["title"])
-
-#     posts = response.json().get("data", {}).get("children", [])
-#     for item in posts:
-#         print(item.get("data", {}).get("title", None))
