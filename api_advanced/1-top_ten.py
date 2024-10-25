fb0a6540ccbde9b8f061178fdc26c36dfe5a1673
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-10-25 15:18:26 +0200

    test one

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,1 +1,22 @@
+#!/usr/bin/python3
+"""
+Description:
+    function that fetched data from a reddit api
+    and prints titles of the first 10 article
+"""
+import requests
 
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    response = requests.get(url, allow_redirects=False)
+    content = response.json()
+    for i in range(10):
+        title = (
+            content.get("data", {})
+            .get("children", [])[i]
+            .get("data", {})
+            .get("title", "No title")
+        )
+        print(title)
